4490a5fc778c7b1be4bd64ac4cdf8e83eeae529b
Pawel Dulak <pawel.dulak@gmail.com>
2023-01-15 13:33:23 +0100

    chore: cleanup

--- cypress/page-objects/hoversPage.js
+++ cypress/page-objects/hoversPage.js
@@ -1,9 +1,7 @@
-const figure = '#hovers-content .figure img';
 const figcaption = '#hovers-content .figcaption';
 
 class HoversPage {
     hoverOverElement() {
-        // cy.get(figure).first().trigger('mouseover');
         cy.get(figcaption).first().should('be.hidden').invoke('show').should('be.visible');
     }
 
